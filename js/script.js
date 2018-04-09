c41097608116fa205b30e7ccfae8b8cb110cbd88
Patrycja Witkowska <narginargila@gmail.com>
2018-04-09 15:32:05 +0200

    Change semicolons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Pole trójkąta
 
-var a = 5;
-	h = 10;
-	areaOfTriangle = a*h/2;
+var a = 5,
+	h = 10,
+	triangleArea = a*h/2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
